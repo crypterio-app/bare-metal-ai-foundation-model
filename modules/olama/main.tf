9b7659f1a9017eceff93ae8a721a38221f4d84fa
Yevgeni Shapiro <23049337+crypterio-app@users.noreply.github.com>
2025-08-29 21:44:25 +0300

    update

---
+++ modules/olama/main.tf
@@ -0,0 +1,28 @@
+
+resource "helm_release" "ollama" {
+  name             = "ollama"
+  repository       = "https://otwld.github.io/ollama-helm/"
+  chart            = "ollama"
+  namespace        = "anythingllm"
+  create_namespace = false
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+
+  set {
+    name  = "service.port"
+    value = "11434"
+  }
+
+  set {
+    name  = "resources.requests.cpu"
+    value = "2"
+  }
+
+  set {
+    name  = "resources.requests.memory"
+    value = "4Gi"
+  }
+}
